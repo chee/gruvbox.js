69d864ac3fd09516bc4b76fe0a7c77d64363ec62
chee <chee@snaek.org>
2017-02-07 20:51:56 +0000

    don't be lonely

---
+++ README.md
@@ -0,0 +1,12 @@
+# gruvbox.js
+
+gruvbox colours as a javascript object
+
+```js
+const {dark0, brightRed, fadedGreen} = require('gruvbox')
+document.body.style.background = dark0
+console.log(fadedGreen) //> #79740e
+```
+
+# credits
+* gruvbox: https://github.com/morhetz/gruvbox
---
+++ index.js
@@ -0,0 +1,43 @@
+module.exports = {
+  dark0: '#282828',
+  dark0Soft: '#32302f',
+  dark1: '#3c3836',
+  dark2: '#504945',
+  dark3: '#665c54',
+  dark4: '#7c6f64',
+
+  gray245: '#928374',
+  gray244: '#928374',
+
+  light0Hard: '#f9f5d7',
+  light0: '#fbf1c7',
+  light0Soft: '#f2e5bc',
+  light1: '#ebdbb2',
+  light2: '#d5c4a1',
+  light3: '#bdae93',
+  light4: '#a89984',
+
+  brightRed: '#fb4934',
+  brightGreen: '#b8bb26',
+  brightYellow: '#fabd2f',
+  brightBlue: '#83a598',
+  brightPurple: '#d3869b',
+  brightAqua: '#8ec07c',
+  brightOrange: '#fe8019',
+
+  neutralRed: '#cc241d',
+  neutralGreen: '#98971a',
+  neutralYellow: '#d79921',
+  neutralBlue: '#458588',
+  neutralPurple: '#b16286',
+  neutralAqua: '#689d6a',
+  neutralOrange: '#d65d0e',
+
+  fadedRed: '#9d0006',
+  fadedGreen: '#79740e',
+  fadedYellow: '#b57614',
+  fadedBlue: '#076678',
+  fadedPurple: '#8f3f71',
+  fadedAqua: '#427b58',
+  fadedOrange: '#af3a03'
+}
