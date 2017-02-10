b20bc04660b33b279455c22063710f2a598b1d5e
chee <chee@snaek.org>
2017-02-10 20:34:20 +0000

    add colour kind objects
    now you can use require('gruvbox').faded.green as well as
    require('gruvbox').fadedGreen

--- index.js
+++ index.js
@@ -1,43 +1,133 @@
+const dark0 = '#282828'
+const dark0Soft = '#32302f'
+const dark1 = '#3c3836'
+const dark2 = '#504945'
+const dark3 = '#665c54'
+const dark4 = '#7c6f64'
+
+const gray245 = '#928374'
+const gray244 = '#928374'
+
+const light0Hard = '#f9f5d7'
+const light0 = '#fbf1c7'
+const light0Soft = '#f2e5bc'
+const light1 = '#ebdbb2'
+const light2 = '#d5c4a1'
+const light3 = '#bdae93'
+const light4 = '#a89984'
+
+const brightRed = '#fb4934'
+const brightGreen = '#b8bb26'
+const brightYellow = '#fabd2f'
+const brightBlue = '#83a598'
+const brightPurple = '#d3869b'
+const brightAqua = '#8ec07c'
+const brightOrange = '#fe8019'
+
+const neutralRed = '#cc241d'
+const neutralGreen = '#98971a'
+const neutralYellow = '#d79921'
+const neutralBlue = '#458588'
+const neutralPurple = '#b16286'
+const neutralAqua = '#689d6a'
+const neutralOrange = '#d65d0e'
+
+const fadedRed = '#9d0006'
+const fadedGreen = '#79740e'
+const fadedYellow = '#b57614'
+const fadedBlue = '#076678'
+const fadedPurple = '#8f3f71'
+const fadedAqua = '#427b58'
+const fadedOrange = '#af3a03'
+
 module.exports = {
-  dark0: '#282828',
-  dark0Soft: '#32302f',
-  dark1: '#3c3836',
-  dark2: '#504945',
-  dark3: '#665c54',
-  dark4: '#7c6f64',
+  dark0Soft,
+  dark0,
+  dark1,
+  dark2,
+  dark3,
+  dark4,
+  dark: {
+    soft: dark0Soft,
+    0: dark0,
+    1: dark1,
+    2: dark2,
+    3: dark3,
+    4: dark4
+  },
 
-  gray245: '#928374',
-  gray244: '#928374',
+  gray244,
+  gray245,
+  gray: {
+    244: gray244,
+    245: gray245
+  },
 
-  light0Hard: '#f9f5d7',
-  light0: '#fbf1c7',
-  light0Soft: '#f2e5bc',
-  light1: '#ebdbb2',
-  light2: '#d5c4a1',
-  light3: '#bdae93',
-  light4: '#a89984',
+  light0Hard,
+  light0Soft,
+  light0,
+  light1,
+  light2,
+  light3,
+  light4,
+  light: {
+    hard: light0Hard,
+    soft: light0Soft,
+    0: light0,
+    1: light1,
+    2: light2,
+    3: light3,
+    4: light4
+  },
 
-  brightRed: '#fb4934',
-  brightGreen: '#b8bb26',
-  brightYellow: '#fabd2f',
-  brightBlue: '#83a598',
-  brightPurple: '#d3869b',
-  brightAqua: '#8ec07c',
-  brightOrange: '#fe8019',
+  brightRed,
+  brightGreen,
+  brightYellow,
+  brightBlue,
+  brightPurple,
+  brightAqua,
+  brightOrange,
+  bright: {
+    red: brightRed,
+    green: brightGreen,
+    yellow: brightYellow,
+    blue: brightBlue,
+    purple: brightPurple,
+    aqua: brightAqua,
+    orange: brightOrange
+  },
 
-  neutralRed: '#cc241d',
-  neutralGreen: '#98971a',
-  neutralYellow: '#d79921',
-  neutralBlue: '#458588',
-  neutralPurple: '#b16286',
-  neutralAqua: '#689d6a',
-  neutralOrange: '#d65d0e',
+  neutralRed,
+  neutralGreen,
+  neutralYellow,
+  neutralBlue,
+  neutralPurple,
+  neutralAqua,
+  neutralOrange,
+  neutral: {
+    red: neutralRed,
+    green: neutralGreen,
+    yellow: neutralYellow,
+    blue: neutralBlue,
+    purple: neutralPurple,
+    aqua: neutralAqua,
+    orange: neutralOrange
+  },
 
-  fadedRed: '#9d0006',
-  fadedGreen: '#79740e',
-  fadedYellow: '#b57614',
-  fadedBlue: '#076678',
-  fadedPurple: '#8f3f71',
-  fadedAqua: '#427b58',
-  fadedOrange: '#af3a03'
+  fadedRed,
+  fadedGreen,
+  fadedYellow,
+  fadedBlue,
+  fadedPurple,
+  fadedAqua,
+  fadedOrange,
+  faded: {
+    red: fadedRed,
+    green: fadedGreen,
+    yellow: fadedYellow,
+    blue: fadedBlue,
+    purple: fadedPurple,
+    aqua: fadedAqua,
+    orange: fadedOrange
+  }
 }
